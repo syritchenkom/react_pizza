e40a0e67474e4952a920031931e85373a11cc436
syritchenkom <syritchenko@gmail.com>
2021-05-14 12:11:12 +0200

    Modified react_pizza folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logoSvg from "./assets/img/pizza-logo.svg";
+import logoSvg from "../assets/img/pizza-logo.svg";
 import Button from "./Button";
 
 function Header() {
