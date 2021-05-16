15c83f39f6c37a68d76658d0eb051afbaefd249d
syritchenkom <syritchenko@gmail.com>
2021-05-16 02:37:23 +0200

    Modified react-pizza folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
+
 import './scss/app.scss';
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
