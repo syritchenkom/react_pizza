c37a09612500889519a4ff4763f3214366feeeea
syritchenkom <syritchenko@gmail.com>
2021-05-22 17:49:16 +0200

    Modified PazzaBlock, Home.jsx and _pizza-block.scss

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-function PizzaBlock({ name }) {
+function PizzaBlock({ name, imageUrl, price }) {
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{name}</h4>
       <div className="pizza-block__selector">
         <ul>
@@ -21,7 +17,7 @@ function PizzaBlock({ name }) {
         </ul>
       </div>
       <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от 395 ₽</div>
+        <div className="pizza-block__price">от {price}</div>
         <div className="button button--outline button--add">
           <svg
             width="12"
