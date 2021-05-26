10e8567ceaa176f56a0482f60e78f0ced2a2c828
syritchenkom <syritchenko@gmail.com>
2021-05-27 01:37:14 +0200

    Refactoring react_pizza

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ import './scss/app.scss';
 import App from './App';
 
 function counter(state = 0, action) {
+  /* if(action.type === 'INCREMENT') {
+        return state + 1;
+    }
+      if(action.type === 'DECREMENT'){
+        return state - 1;
+      }
+      return state;
+  }) */
   switch (action.type) {
     case 'INCREMENT':
       return state + 1;
@@ -17,6 +25,18 @@ function counter(state = 0, action) {
       return state;
   }
 }
+
+const store = createStore(counter);
+
+//store.subscribe(() => console.log('хранилище изменилось', store.getState()));
+
+console.log(store.getState()); // 0
+
+store.dispatch({ type: 'INCREMENT' }); // 0 => 1
+store.dispatch({ type: 'INCREMENT' }); // 1 => 2
+store.dispatch({ type: 'DECREMENT' }); // 2 => 1
+
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
