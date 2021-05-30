c3f2abc2e0eaae0580f91188a44b3b8ddf004e66
syritchenkom <syritchenko@gmail.com>
2021-05-30 23:04:51 +0200

    Refactoring App.js

--- src/App.js
+++ src/App.js
@@ -7,23 +7,7 @@ import { Header } from './components';
 import { Home, Cart } from './pages';
 import { setPizzas } from './redux/actions/pizzas';
 
-/* function App() {
-  React.useEffect(() => {
-    axios.get('http://localhost:3000/db.json').then(({ data }) => {
-      // console.log('axios', resp);
-      setPizzas(data.pizzas);
-    });
 
-     fetch('http://localhost:3000/db.json')
-      .then((resp) => {
-        console.log('fetch', resp);
-        return resp.json();
-      })
-      .then((json) => {
-        setPizzas(json.pizzas);
-      }); 
-  }, []);
-} */
 
 class App extends React.Component {
   componentDidMount() {
