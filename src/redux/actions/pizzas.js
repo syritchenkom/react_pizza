2ba50d58733de7e54b97a3383f77c8b50fc1da9b
syritchenkom <syritchenko@gmail.com>
2021-06-27 12:20:16 +0200

    Modified action/pizzas.js

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -4,7 +4,7 @@ export const setLoaded = (payload) => ({
   type: 'SET_LOADED',
   payload,
 });
-
+//commit
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
   dispatch({
     type: 'SET_LOADED',
@@ -12,9 +12,9 @@ export const fetchPizzas = (sortBy, category) => (dispatch) => {
   });
   axios
     .get(
-      `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${
-        sortBy.type
-      }&_order=${sortBy.order}`,
+      `/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${
+        sortBy.order
+      }`,
     )
     .then(({ data }) => {
       dispatch(setPizzas(data));
